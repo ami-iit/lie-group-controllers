3b29b300400941684cd009a534839f411f670fc9
Giulio Romualdi <giulio.romualdi@gmail.com>
2020-08-01 19:42:11 +0200

    Add CMake skeleton

---
+++ cmake/LieGroupControllersDependencies.cmake
@@ -0,0 +1,7 @@
+# Authors: Giulio Romualdi
+# CopyPolicy: Released under the terms of the LGPLv2.1 or later
+
+#---------------------------------------------
+## Required Dependencies
+find_package(Eigen3 3.2.92 REQUIRED)
+find_package(manif REQUIRED)
