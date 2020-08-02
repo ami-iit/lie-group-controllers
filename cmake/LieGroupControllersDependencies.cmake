e110415b07e1548a6201701e9235ffacdb1b1b03
Giulio Romualdi <giulio.romualdi@gmail.com>
2020-08-02 17:31:07 +0200

    Add cmake utilities

--- cmake/LieGroupControllersDependencies.cmake
+++ cmake/LieGroupControllersDependencies.cmake
@@ -1,7 +1,17 @@
 # Authors: Giulio Romualdi
 # CopyPolicy: Released under the terms of the LGPLv2.1 or later
 
+include(LieGroupControllersFindOptionalDependencies)
+
 #---------------------------------------------
 ## Required Dependencies
 find_package(Eigen3 3.2.92 REQUIRED)
 find_package(manif REQUIRED)
+
+#---------------------------------------------
+## Optional Dependencies
+find_package(Catch2 QUIET)
+checkandset_optional_dependency(Catch2)
+
+find_package(VALGRIND QUIET)
+checkandset_optional_dependency(VALGRIND)
