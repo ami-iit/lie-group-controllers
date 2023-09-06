50078d5b682ad863adbfc31de2acfb83aa536c72
Silvio Traversaro <silvio@traversaro.it>
2023-09-06 22:52:08 +0200

    License the repo under the BSD-3-Clause license (#14)

--- cmake/AddUninstallTarget.cmake
+++ cmake/AddUninstallTarget.cmake
@@ -11,20 +11,8 @@
 # custom target uninstall that will remove the files installed by your package
 # (using install_manifest.txt)
 
-#=============================================================================
-# Copyright 2008-2013 Kitware, Inc.
-# Copyright 2013 iCub Facility, Istituto Italiano di Tecnologia
-#   Authors: Daniele E. Domenichelli <daniele.domenichelli@iit.it>
-#
-# Distributed under the OSI-approved BSD License (the "License");
-# see accompanying file Copyright.txt for details.
-#
-# This software is distributed WITHOUT ANY WARRANTY; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the License for more information.
-#=============================================================================
-# (To distribute this file outside of CMake, substitute the full
-#  License text for the above reference.)
+# SPDX-FileCopyrightText: Fondazione Istituto Italiano di Tecnologia (IIT)
+# SPDX-License-Identifier: BSD-3-Clause
 
 
 if(DEFINED __ADD_UNINSTALL_TARGET_INCLUDED)
